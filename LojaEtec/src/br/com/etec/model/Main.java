9469c71a5ac225207e8221aae67b7b42b5b99715
lab04 <lab04@lab4-14>
2024-08-19 20:32:18 -0300

    Protótipo da tela de login.
    Adicionando campos de texto e botões.
    Sem verificação de acesso.

--- LojaEtec/src/br/com/etec/model/Main.java
+++ LojaEtec/src/br/com/etec/model/Main.java
@@ -2,6 +2,7 @@ package br.com.etec.model;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
@@ -15,12 +16,17 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("/br/com/etec/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			
+			
+			
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
